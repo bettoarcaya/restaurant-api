ff008b8773c63050e018d60b70927fc8b02cf22a
Humberto Arcaya <betoarcaya@gmail.com>
2022-09-14 21:16:12 -0400

    feat: menu module added

---
+++ src/menu/menu.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Menu } from './entities/menu.entity';
+
+@Injectable()
+export class MenuService {
+  constructor(
+    @InjectRepository(Menu)
+    private menuRepository: Repository<Menu>,
+  ) {}
+}
